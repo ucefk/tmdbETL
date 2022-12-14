72bf80d4d35f386492775e1dde5b684a0cc74a4f
Youcef <ucef.kharroubi@gmail.com>
2022-12-14 15:03:16 +0100

    feature: implement a frontend for the data analysis module

---
+++ data_analysis/static/js/script.js
@@ -0,0 +1,7 @@
+// script.js
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Code JavaScript à exécuter une fois que la page est chargée
+    console.log('Script loaded!');
+  });
+  
